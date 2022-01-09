49ee1570e8e36d57cb6b991c03b6120b7972a8c3
lishii228 <96789018+lishii228@users.noreply.github.com>
2022-01-09 14:45:28 +0300

    Update and rename sinus.py to cosinus.py

--- cosinus.py
+++ cosinus.py
@@ -1,16 +1,13 @@
 import math
-import matplotlib.pyplot as plt
 
-import numpy as np
 
 def factorial(x):
 	t=1
 	for k in range(1,x+1):
 		t*=k
 	return t
-def sin(x):
+def cos(x):
 	a=0
 	for k in range(10):
-		a+= ((-1)**k)*((x**(2*k+1))/(factorial(2*k+1)))
+		a+= ((-1)**k)*((x**(2*k))/(factorial(2*k)))
 	return a
-
